00c14c7dff116256b43bb8536b04e324b4e7c933
LuisGonzalez27 <luis27coding@gmail.com>
2022-11-08 15:28:09 +0100

    test stampa email

--- js/script.js
+++ js/script.js
@@ -1,1 +1,20 @@
 const {createApp} = Vue
+
+createApp({
+    data(){
+        return{
+            valoreApi: [],
+        }
+    },
+    methods:{
+        callApi(){
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((email)=>{
+                console.log(email.data.response);
+                this.valoreApi.push(email.data.response);
+            })
+        }
+    },
+    created(){
+        this.callApi();
+    }
+}).mount('#app');
